01c7286b8c5bf73c9e87bfe8bf38edb9683427c7
andre <codigoaberto.info@gmail.com>
2020-01-02 16:21:59 -0300

    chore: add actions task

--- client/src/components/task/TaskItem.js
+++ client/src/components/task/TaskItem.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { Card, CardContent } from "@material-ui/core";
+import { Card, CardContent,
+        CardActions, Grid,
+        FormControl, Select,
+        MenuItem, IconButton
+} from "@material-ui/core";
+import { Delete } from "@material-ui/icons";
 import styled from "styled-components";
 
 const TaskContainer = styled.div`
@@ -14,6 +19,28 @@ const TaskItem = ({ task }) => {
                 <h3>{ task.title }</h3>
                 <p>{ task.description }</p>
             </CardContent>
+            <CardActions style={{ padding: '14px' }} disableSpacing>
+                <Grid justify="space-between" container>
+                    <Grid item>
+                        <FormControl style={{ width: '140px' }}>
+                            <Select
+                                value={task.status}
+                                displayEmpty
+                            >
+                                <MenuItem value={'OPEN'}>Aberta</MenuItem>
+                                <MenuItem value={'IN_PROGRESS'}>Em Progresso</MenuItem>
+                                <MenuItem value={'DONE'}>Finalizada</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item>
+                        <IconButton>
+                            <Delete color="error" />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </CardActions>
         </Card>
         </TaskContainer>
     )
