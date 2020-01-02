2918fc760b787cdb7ea1ca8e4a543797626a9194
andre <codigoaberto.info@gmail.com>
2020-01-02 19:54:32 -0300

    feature: add dockerfile and docker-compose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
+  const port = process.env.PORT || 8000;
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(port);
+  logger.log(`Application running on port: ${port}`);
 }
 bootstrap();
